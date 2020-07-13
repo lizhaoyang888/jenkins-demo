45eb175f022da6adb70d0962bd13f40016cdc969
lichaoyang1 <lichaoyang1@guazi.com>
2020-07-13 14:10:08 +0800

    update threadPool coreSize

--- src/main/java/com/example/jenkinsdemo/config/ThreadPoolConfig.java
+++ src/main/java/com/example/jenkinsdemo/config/ThreadPoolConfig.java
@@ -21,20 +21,18 @@ public class ThreadPoolConfig {
         ThreadFactory threadFactory = new ThreadFactoryBuilder()
                 .setNameFormat("thread-pool-%d").build();
 
-        try {
-            int cpuNumber = Runtime.getRuntime().availableProcessors();
-            return new ThreadPoolExecutor(
-                    4,4,
-                    300,
-                    TimeUnit.SECONDS,
-                    new LinkedBlockingQueue<>(10240),
-                    threadFactory,
-                    new ThreadPoolExecutor.AbortPolicy()
-            );
-        }catch (Exception e){
-            log.info("我知道哪儿错了");
-        }
-        return null;
+        //阿里云服务器的CPU核心数量为1
+        int cpuNumber = Runtime.getRuntime().availableProcessors();
+
+        return new ThreadPoolExecutor(
+                (cpuNumber * 2) + 4,
+                (cpuNumber * 2) + 8,
+                300,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(10240),
+                threadFactory,
+                new ThreadPoolExecutor.AbortPolicy()
+        );
     }
 
 }
